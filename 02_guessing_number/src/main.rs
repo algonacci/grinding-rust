1c13d3f3dc4ed567b73d981c0e04d351a3a1ac7f
Eric Julianto <86970816+algonacci@users.noreply.github.com>
2025-01-26 18:16:22 +0700

    optimize rust idioms

--- 02_guessing_number/src/main.rs
+++ 02_guessing_number/src/main.rs
@@ -1,39 +1,55 @@
 use rand::Rng;
+use std::cmp::Ordering;
 use std::io;
+
+/// Membaca input dari pengguna dan mengonversinya ke i32.
+/// Jika input tidak valid, mengembalikan `None`.
+fn read_input() -> Option<i32> {
+    let mut input = String::new();
+    io::stdin()
+        .read_line(&mut input)
+        .expect("Gagal membaca input");
+
+    input.trim().parse().ok()
+}
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
     println!("Permainan menebak angka dimulai!");
     println!("Masukkan Angka dari 1-100:");
 
-    let secret_number = rand::thread_rng().gen_range(1..101);
+    // Generate angka acak antara 1 dan 100
+    let secret_number = rand::thread_rng().gen_range(1..=100);
 
     loop {
-        let mut input = String::new();
-        io::stdin().read_line(&mut input)?;
-
-        let input: i32 = match input.trim().parse() {
-            Ok(num) => num,
-            Err(_) => {
+        // Membaca input dari pengguna
+        let input = match read_input() {
+            Some(num) => num,
+            None => {
                 println!("Input tidak valid, masukkan angka!");
                 continue;
             }
         };
 
-        if input < secret_number {
-            if secret_number - input <= 10 {
-                println!("Terlalu kecil, tapi mendekati!");
-            } else {
-                println!("Terlalu kecil!");
+        // Memeriksa tebakan pengguna
+        match input.cmp(&secret_number) {
+            Ordering::Less => {
+                if secret_number - input <= 10 {
+                    println!("Terlalu kecil, tapi mendekati!");
+                } else {
+                    println!("Terlalu kecil!");
+                }
             }
-        } else if input > secret_number {
-            if input - secret_number <= 10 {
-                println!("Terlalu besar, tapi mendekati!");
-            } else {
-                println!("Terlalu besar!");
+            Ordering::Greater => {
+                if input - secret_number <= 10 {
+                    println!("Terlalu besar, tapi mendekati!");
+                } else {
+                    println!("Terlalu besar!");
+                }
             }
-        } else {
-            println!("Selamat, tebakanmu benar!");
-            break;
+            Ordering::Equal => {
+                println!("Selamat, tebakanmu benar!");
+                break;
+            }
         }
     }
 
